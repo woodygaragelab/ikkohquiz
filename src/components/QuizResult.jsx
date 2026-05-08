1d83f27e8eeeeee7d39d5084f9ebf3f6bc70606f
woodygaragelab <woodygaragelab@gmail.com>
2026-05-08 15:49:03 +0900

    レイアウト修正

--- src/components/QuizResult.jsx
+++ src/components/QuizResult.jsx
@@ -1,8 +1,8 @@
 const QuizResult = ({ score, total, onNext, currentGroup }) => {
   return (
     <div className="main">
-      <h2>{currentGroup}</h2>
-      <h2>正解率:{Math.round(score / total * 100)}%  ({score} / {total})</h2>
+      <h2 style={{ padding: '0 16px' }}>{currentGroup}</h2>
+      <h2 style={{ padding: '0 16px' }}>正解率:{Math.round(score / total * 100)}%  ({score} / {total})</h2>
     </div>
   );
 };
